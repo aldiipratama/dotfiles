c32d16ae6f3b6e870bc961e7e23b37a23f688bc1
aldiipratama <paldi0013@gmail.com>
2026-07-08 11:51:44 +0700

    refactor(missing): delete directory alacritty, bspwm, polybar. missing place

--- bspwm/scripts/_autostart.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pgrep -x sxhkd >/dev/null || sxhkd -c $HOME/.config/bspwm/sxhkdrc &
-
-$HOME/.config/bspwm/scripts/monitor.sh
-$HOME/.config/polybar/scipts/launch.sh
-$HOME/.config/bspwm/scripts/wallpaper.sh
--- bspwm/scripts/wallpaper.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-feh --bg-fill $HOME/Pictures/Wallpapers/images/4k-keyboard.jpg &
